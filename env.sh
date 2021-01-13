75e661aced0d8bbd2065993264be2a9dc8b394c5
Benoit Sida <benoit.sida@gmail.com>
2021-01-13 11:05:17 -0500

    fix echo command; add preprod substitution

--- env.sh
+++ env.sh
@@ -5,6 +5,7 @@ command=${arrIN[0]}
 env_name=${arrIN[1]}
 
 dev_sub=${NPM_RUN_ENV_DEV_SUBSTITUTION:-development}
+preprod_sub=${NPM_RUN_ENV_PREPROD_SUBSTITUTION:-preproduction}
 prod_sub=${NPM_RUN_ENV_PROD_SUBSTITUTION:-production}
 local_sub=${NPM_RUN_ENV_LOCAL_SUBSTITUTION:-local}
 test_sub=${NPM_RUN_ENV_TEST_SUBSTITUTION:-test}
@@ -32,34 +33,43 @@ else
         then
             echo "Error: No env specified"
         else
-            echo "Running $command for environment $dev_sub"
             case $env_name in
                 dev)
-                        NODE_ENV=$dev_sub && npm run ${@:-$command}
-                        ;;
+                    echo "Running $command for environment $dev_sub"
+                    NODE_ENV=$dev_sub && npm run ${@:-$command}
+                    ;;
                 prod)
-                        NODE_ENV=$prod_sub && npm run ${@:-$command}
-                        ;;
+                    echo "Running $command for environment $prod_sub"
+                    NODE_ENV=$prod_sub && npm run ${@:-$command}
+                    ;;
+                preprod)
+                    echo "Running $command for environment $preprod_sub"
+                    NODE_ENV=$preprod_sub && npm run ${@:-$command}
+                    ;;
                 local)
-                        NODE_ENV=$local_sub && npm run ${@:-$command}
-                        ;;
+                    echo "Running $command for environment $local_sub"
+                    NODE_ENV=$local_sub && npm run ${@:-$command}
+                    ;;
                 test)
-                        NODE_ENV=$test_sub && npm run ${@:-$command}
-                        ;;
+                    echo "Running $command for environment $test_sub"
+                    NODE_ENV=$test_sub && npm run ${@:-$command}
+                    ;;
                 ci)
-                        NODE_ENV=$ci_sub && npm run ${@:-$command}
-                        ;;
+                    echo "Running $command for environment $ci_sub"
+                    NODE_ENV=$ci_sub && npm run ${@:-$command}
+                    ;;
                 *)
-                        upper_custom=$(echo $env_name |tr [a-z] [A-Z])
-                        custom_sub="NPM_RUN_ENV_${upper_custom}_SUBSTITUTION"
-                        if [ -z ${!custom_sub} ]
-                        then
-                            echo "No custom substitution for $env_name : You can define it in the env var NPM_RUN_ENV_${upper_custom}_SUBSTITUTION"
-                        else
-                            echo "Found custom substitution for $env_name : ${!custom_sub}"
-                            NODE_ENV=${!custom_sub} && npm run ${@:-$command}
-                        fi
-                        ;;
+                    upper_custom=$(echo $env_name |tr [a-z] [A-Z])
+                    custom_sub="NPM_RUN_ENV_${upper_custom}_SUBSTITUTION"
+                    if [ -z ${!custom_sub} ]
+                    then
+                        echo "No custom substitution for $env_name : You can define it in the env var NPM_RUN_ENV_${upper_custom}_SUBSTITUTION"
+                    else
+                        echo "Found custom substitution for $env_name : ${!custom_sub}"
+                        echo "Running $command for environment ${!custom_sub}"
+                        NODE_ENV=${!custom_sub} && npm run ${@:-$command}
+                    fi
+                    ;;
             esac
         fi
     fi
